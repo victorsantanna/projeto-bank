bb726ecb5aad2d0fe171e7c2a8119ba33e045de7
victorsantanna <victor.esef@hotmail.com>
2024-08-30 07:57:42 -0300

    feat: adiciona rota

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,13 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../feature/Login/LoginProjeto.vue')
-  }
+  },
+  {
+    path: '/conta-usuario',
+    name: 'conta-usuario',
+    component: () => import(/* webpackChunkName: "about" */ '../feature/Conta/ContaPrincipal.vue')
+  },
 ]
 
 const router = createRouter({
