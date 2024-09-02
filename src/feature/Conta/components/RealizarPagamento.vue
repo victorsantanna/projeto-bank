35a51adfbb7f29d662f0780bbceb3b9452a58d28
victorsantanna <victor.esef@hotmail.com>
2024-09-02 07:19:33 -0300

    style: adiciona estilização

--- src/feature/Conta/components/RealizarPagamento.vue
+++ src/feature/Conta/components/RealizarPagamento.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="container">
     <div class="conteudo-info">
-        <p>Realizar Pagamento</p>
+      <p>Realizar Pagamento</p>
+      <div class="conteudo-info-botoes">
         <button>Fazer transferência</button>
         <button>Transação de Pix</button>
-        <a href="">Conferir dados da conta</a>
+      </div>
+      <a href="">Conferir dados da conta</a>
 
     </div>
   </div>
@@ -15,14 +17,51 @@
 </script>
 
 <style scoped>
-.container{
-    max-width: 400px;
-   
-   ;
+.container {
+  max-width: 400px;
+  width: 300px;
+  
+  
 }
-.conteudo-info{
-    background-color: #FEAA12 ;
-    border: 1px solid #F18229;
-    border-radius: 8px
+
+.conteudo-info {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  background-color: #FEAA12;
+  border: 1px solid #F18229;
+  border-radius: 8px;
+  padding: 30px;
+  
+}
+
+.conteudo-info p{
+  color: #FFFFFF;
+}
+
+.conteudo-info a{
+  font-size: 12px;
+  color: var(--primary-color);
+  font-weight: 600;
+  text-align: end;
+}
+
+.conteudo-info-botoes{
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.conteudo-info-botoes button{
+  border-radius: 8px;
+  border: none;
+  font-weight: 600;
+  height: 24px;
+  text-align: left;
+  padding-left: 8px;
+  background-image: url('@/assets/img/Conta/seta.png'); /* URL da imagem da seta */
+  background-repeat: no-repeat;
+  background-position:right; 
+  
 }
 </style>
