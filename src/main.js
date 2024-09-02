8808f81734c2c05303440e251949e456c2e3f62b
victorsantanna <victor.esef@hotmail.com>
2024-09-02 07:17:31 -0300

    feat: importa o axios

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/css/variables.css'
+import axios from 'axios'
 
-createApp(App).use(router).mount('#app')
+
+createApp(App).use(router).mount('#app').app.config.globalProperties.$axios = axios;
