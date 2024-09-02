d7f64141305410dbcc77bacfd342efdca09e2429
victorsantanna <victor.esef@hotmail.com>
2024-09-02 07:19:14 -0300

    feat: adiciona o botão histórico

--- src/feature/Conta/components/MovimentacaoSaldo.vue
+++ src/feature/Conta/components/MovimentacaoSaldo.vue
@@ -2,13 +2,23 @@
     <div class="container">
         <div class="conteudo-saldo">
             <p>Ultimas movimentações</p>
-            <div>
+            <div class="conteudo-movimentacao">
                 <h4>01 de agosto</h4>
                 <div class="conteudo-saldo-info">
                     <p>Maria</p>
                     <p>R$ 250,00</p>
                 </div>
             </div>
+            <div class="conteudo-movimentacao">
+                <h4>01 de agosto</h4>
+                <div class="conteudo-saldo-info">
+                    <p>Maria</p>
+                    <p>R$ 250,00</p>
+                </div>
+            </div>
+          <div>
+            <button>Conferir Histórico Completo</button>
+          </div>
 
         </div>
     </div>
@@ -20,13 +30,18 @@
 
 <style scoped>
 .container {
-    max-width: 400px;
+    max-width: 600px;
     display: flex;
     flex-direction: column;
     justify-content: center;
 }
 
+.conteudo-movimentacao{
+    margin-top: 30px;
+}
+
 .conteudo-saldo {
+    width: 600px;
     border: 2px solid #FEAA12;
     border-radius: 8px;
     padding: 15px ;
